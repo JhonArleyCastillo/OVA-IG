4f0c5b4eb07631e2ed72f3781c6089563307da66
Jhon-Arley-Castillo-Vitovis <jhona_castillov@soy.sena.edu.co>
2024-03-31 15:15:03 -0500

    Se agregan menus lateral y horizontal

---
+++ java-script.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", function(event) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+});
